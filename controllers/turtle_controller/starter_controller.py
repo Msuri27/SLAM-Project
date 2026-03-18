b9f88cc424d85888508844d88d465ce92aebe680
Msuri27 <matthewsuri@gmail.com>
2026-03-17 22:40:29 -0500

    built helper functions for pose prop and keyframe decision making. implemented logic for initiating landmarks. added obs and odom factors to keep track of them in the graph

--- controllers/turtle_controller/starter_controller.py
+++ controllers/turtle_controller/starter_controller.py
@@ -6,8 +6,67 @@ import numpy as np
 
 class StudentController:
     def __init__(self):
-        pass
+        # init starting robot pose
+        self.poses = []
+        self.current_pose = np.array([0.0, 0.0, 0.0], dtype=float)
+        self.poses.append(self.current_pose)
 
+        # landmark storage
+        self.landmarks = {}
+        
+        # accumulated ds and dtheta between keyframes
+        self.accumulated_ds = 0.0
+        self.accumulated_dtheta = 0.0
+
+        # graph factors
+        self.prior_factors = []
+        self.odom_factors = []
+        self.obs_factors = []
+
+        # key frame and optimization params
+        self.translation_threshold = 0.05
+        self.rotation_threshold = 0.1
+        self.optimize_every = 5
+        
+    # HELPER METHODS:
+
+    # angle wrapper helper
+    def wrap_angle(self, theta):
+        return np.arctan2(np.sin(theta), np.cos(theta))
+    
+    # propagate pose through diff drive motion model (may want to replace with EKF later)
+    def propagate_pose(self, pose: np.ndarray, ds: float, dtheta: float) -> np.ndarray:
+        x, y, theta = pose
+
+        x_new = x + ds * np.cos(theta + 0.5 * dtheta)
+        y_new = y + ds * np.sin(theta + 0.5 * dtheta)
+        theta_new = self.wrap_angle(theta + dtheta)
+
+        return np.array([x_new, y_new, theta_new], dtype=float)
+    
+    def keyframe_decision(self, last_pose: np.ndarray, current_pose: np.ndarray, ds_thresh: float, dtheta_thresh: float):
+        dx = current_pose[0] - last_pose[0]
+        dy = current_pose[1] - last_pose[1]
+
+        dtheta = self.wrap_angle(current_pose[2] - last_pose[2])
+        ds = np.hypot(dx, dy)
+
+        return (ds > ds_thresh) or (dtheta > dtheta_thresh)
+    
+    # TODO: may need to investigate bearing phi
+    def intit_landmark(self, landmark_id, pose: np.ndarray, r: float, phi: float):
+        x, y, theta = pose
+
+        mx = x + r * np.cos(theta + phi)
+        my = y + r * np.sin(theta + phi)
+
+        return np.array([mx, my], dtype=float)
+    
+    # TODO IMPLEMENT LATER
+    def run_graph_optimization(self):
+        pass
+    
+    # LOOP:
     def step(self, sensors):
         """
         Compute robot control as a function of sensors.
@@ -30,7 +89,60 @@ class StudentController:
         control_dict["left_motor"] = 3.0
         control_dict["right_motor"] = 3.0
 
+        # TODO: change later
         estimated_pose = [0, 0, 0]
         estimated_map = {"BOX_1": [0.5, 0.5]}
+
+        # ds and dtheta from odometry
+        ds = sensors["odometry"][0]
+        dtheta = sensors["odometry"][1]
+
+        # accumulated ds and dtheta
+        self.accumulated_ds += ds
+        self.accumulated_dtheta += dtheta
+        self.accumulated_dtheta = self.wrap_angle(self.accumulated_dtheta)
+
+        # update current_pose using motion model
+        self.current_pose = self.propagate_pose(self.current_pose, ds, dtheta)
+
+        # when new keyframe threshold reached
+        if self.keyframe_decision(self.poses[-1], self.current_pose, self.translation_threshold, self.rotation_threshold):
+            # calculate indices for last and new keyframe poses and add a copy of the current pose to poses
+            prev_pose_idx = len(self.poses) - 1
+            self.poses.append(self.current_pose.copy())
+            current_pose_idx = len(self.poses) - 1
+
+            # build odom factor dictionary to track last keyframe pose and new keyframe pose and difference between
+            new_odom_factor = {}
+            new_odom_factor["i"] = prev_pose_idx
+            new_odom_factor["j"] = current_pose_idx
+            new_odom_factor["measurement"] = np.array([self.accumulated_ds, 0.0, self.accumulated_dtheta], dtype=float)
+
+            # add new odom factor to global list
+            self.odom_factors.append(new_odom_factor)
+
+            # reset accumulation for next keyframe
+            self.accumulated_ds = 0.0
+            self.accumulated_dtheta = 0.0
+
+            # scan for landmarks
+            for landmark_id, (r_meas, phi_meas) in sensors["observed_landmarks"].items():
+                if landmark_id not in self.landmarks:
+                    # add landmark to global dictionary
+                    self.landmarks[landmark_id] = self.intit_landmark(landmark_id, self.current_pose, r_meas, phi_meas)
+
+                # build obs factor dictionary to track current observations and measurement
+                new_obs_factor = {}
+                new_obs_factor["pose_idx"] = current_pose_idx
+                new_obs_factor["landmark_id"] = landmark_id
+                new_obs_factor["measurement"] = np.array([r_meas, phi_meas], dtype=float)
+                self.obs_factors.append(new_obs_factor)
+
+            if len(self.poses) % self.optimize_every == 0:
+                self.run_graph_optimization()
+
+
+        estimated_pose = self.current_pose
+        estimated_map = self.landmarks
 
         return control_dict, estimated_pose, estimated_map
